dcac3b9cbfd4c1675fe6e4f2fbf59fd7114db5c8
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2017-05-11 00:21:31 +0000

    pos_p.sh -> positional parameters for shell scripts

---
+++ shelltesting/pos_p.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+VAR_1=$1
+VAR_2=$2
+VAR_3=$3
+VAR_4=$4
+
+echo $VAR_1
+echo $VAR_2
+echo $VAR_3
+echo $VAR_4
+
+#running ./pos_p.sh one two three four will result with
+#one
+#two
+#three
+#four
+#printed to the terminal
