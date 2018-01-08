d23d1778a110cecf8160fb9ef33f7bccaa156b69
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2018-01-08 22:16:37 +0000

    Added comment on tty.js user_data

--- boto3tests/devbox_setup.py
+++ boto3tests/devbox_setup.py
@@ -72,6 +72,8 @@ user_data = """#!/bin/bash
 git clone https://github.com/yevgenybulochnik/ttyjs-provisioner.git /tmp/tp
 ./tmp/tp/ttyjs_provisioner.sh yevgeny 12345 yevgenybulochnik
 """
+# Note change hardcoded tty.js variables when running this script. Future update will obfuscate these variables
+
 instances = ec2.create_instances(
         ImageId='ami-0def3275',
         MinCount=1,
