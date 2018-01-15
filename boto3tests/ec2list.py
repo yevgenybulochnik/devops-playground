a3d5e91d1da043722bab1e218a927ab6ce2a98ca
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2018-01-15 21:00:29 +0000

    Change to dictionary data structure

--- boto3tests/ec2list.py
+++ boto3tests/ec2list.py
@@ -6,25 +6,17 @@ reg_name = 'us-west-2'
 ec2 = boto3.resource('ec2', region_name=reg_name)
 
 data = []
-header = ['Name', 'State', 'Sec_Gr', 'ID', 'Type', 'Pub IP']
 for instance in ec2.instances.filter():
-    tags = instance.tags
-    security_groups = instance.security_groups
-    instance_sec_group = security_groups[0]['GroupName']
-    instance_id = instance.id
-    instance_type = instance.instance_type
-    instance_name = tags[0]['Value']
-    instance_state = instance.state
-    instance_pub_ip = instance.public_ip_address
-    data.append([
-        instance_name,
-        instance_state['Name'],
-        instance_sec_group,
-        instance_id,
-        instance_type,
-        instance_pub_ip])
+    data.append({
+        'Name': instance.tags[0]['Value'],
+        'State': instance.state['Name'],
+        'Sec_Gr': instance.security_groups[0]['GroupName'],
+        'Id': instance.id,
+        'Type': instance.instance_type,
+        'Pub IP': instance.public_ip_address
+    })
 
 os.system('clear')
 print(f'AWS Console  --{reg_name}--\n')
-print(tabulate(data, headers=header))
+print(tabulate(data, headers='keys', tablefmt='simple'))
 print()
