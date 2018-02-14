9f8acbeb6eac6959d3c0ec1e0b34da1705ef74e6
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2018-02-14 22:17:29 +0000

    docker tutorial, get and install

---
+++ docker-tutorial/install_docker.sh
@@ -0,0 +1,6 @@
+# Install docker from deb package
+# dists located at https://download.docker.com/linux/ubuntu/dists {distribution} pool/stable --> amd64
+
+wget https://download.docker.com/linux/ubuntu/dists/xenial/pool/stable/amd64/docker-ce_17.03.0~ce-0~ubuntu-xenial_amd64.deb
+
+sudo dpkg -i docker-ce_17.03.0~ce-0~ubuntu-xenial_amd64.deb
