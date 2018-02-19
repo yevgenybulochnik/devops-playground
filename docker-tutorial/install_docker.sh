aa62e35f8efad8ab295ddfce2d7fbfae07553db0
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2018-02-18 16:07:04 -0800

    Add user to docker group

--- docker-tutorial/install_docker.sh
+++ docker-tutorial/install_docker.sh
@@ -4,3 +4,6 @@
 wget https://download.docker.com/linux/ubuntu/dists/xenial/pool/stable/amd64/docker-ce_17.03.0~ce-0~ubuntu-xenial_amd64.deb
 
 sudo dpkg -i docker-ce_17.03.0~ce-0~ubuntu-xenial_amd64.deb
+
+USER=$(whoami)
+sudo usermod -aG docker $USER
