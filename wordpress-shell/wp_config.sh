acf527f8efafd9ade5b053e0d821b745491ea047
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2018-04-03 20:55:50 -0700

    Wordpress mysql db setup

--- wordpress-shell/wp_config.sh
+++ wordpress-shell/wp_config.sh
@@ -13,6 +13,11 @@ echo "mysql-server mysql-server/root_password_again password rootpassword" | deb
 # Install MySQL
 apt install mysql-server -y
 
+# Create wordpress db
+mysql -u root -prootpassword -e "CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
+mysql -u root -prootpassword -e "GRANT ALL ON wordpress.* TO 'wordpressuser'@'localhost' IDENTIFIED BY 'password';"
+mysql -u root -prootpassword -e "FLUSH PRIVILEGES;"
+
 # Install PHP
 apt install -y php \
     libapache2-mod-php \
@@ -31,7 +36,7 @@ echo "
 </IFModule>
 " > /etc/apache2/mods-enabled/dir.conf
 
-# PHPmyadmin config qusetions
+# PHPmyadmin config questions
 echo 'phpmyadmin phpmyadmin/dbconfig-install boolean true' | debconf-set-selections
 echo 'phpmyadmin phpmyadmin/app-password-confirm password phpmyadminpass' | debconf-set-selections
 echo 'phpmyadmin phpmyadmin/mysql/admin-pass password rootpassword' | debconf-set-selections
