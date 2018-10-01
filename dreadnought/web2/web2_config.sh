ff75a9c31bbc985c397285307c04358241d96b4d
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2018-09-30 19:53:25 -0700

    Nodebb setup

--- dreadnought/web2/web2_config.sh
+++ dreadnought/web2/web2_config.sh
@@ -9,4 +9,12 @@ apt install nodejs -y
 # Get nodebb
 cd /home/$VAGRANT_USER
 sudo -iu $VAGRANT_USER git clone -b v1.10.x https://github.com/NodeBB/NodeBB.git nodebb
-cd nodebb
+
+# Add config.json and execute setup
+cp /vagrant/web2/config.json /home/$VAGRANT_USER/nodebb/
+cd nodebb/
+chown $VAGRANT_USER:$VAGRANT_USER config.json
+su - $VAGRANT_USER -c "cd /home/$VAGRANT_USER/nodebb;./nodebb setup"
+
+# Start nodebb
+su - $VAGRANT_USER -c "cd /home/$VAGRANT_USER/nodebb;./nodebb start"
