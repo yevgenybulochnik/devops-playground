22d814b9fd634907c92e41da8369620ffc38edb4
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2018-12-01 15:52:47 -0800

    Example template for loop

--- flask-tutorial/app/routes.py
+++ flask-tutorial/app/routes.py
@@ -6,4 +6,14 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username': 'Eugene'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': "It's a beautiful day in portland"
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': "The Avengers movie was awesome!"
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
