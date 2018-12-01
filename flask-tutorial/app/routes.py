7dfb02bb76e2626a465b2c164b2fb6fe66937d82
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2018-12-01 15:30:39 -0800

    Example inline template with route

--- flask-tutorial/app/routes.py
+++ flask-tutorial/app/routes.py
@@ -4,4 +4,15 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello, world'
+    user = {'username': 'Eugene'}
+    return f'''
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Home Page -- Flask Tutorial</title>
+</head>
+<body>
+   <h1>Hello, {user["username"]}!</h1>
+</body>
+</html>
+    '''
