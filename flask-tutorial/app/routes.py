1d01a74c59059d7f761ab69e563b417807fd7368
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2018-12-04 21:53:34 -0800

    New user registration setup

--- flask-tutorial/app/routes.py
+++ flask-tutorial/app/routes.py
@@ -1,8 +1,8 @@
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
-from app import app
-from app.forms import LoginForm
+from app import app, db
+from app.forms import LoginForm, RegistartionForm
 from app.models import User
 
 # urllib.parse.unquote_plus required when flask is behind an apache server,
@@ -49,3 +49,17 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistartionForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
