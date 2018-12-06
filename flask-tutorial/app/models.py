607fabc0fcc341016f9eb5245c4eff2497a21499
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2018-12-05 20:25:13 -0800

    User profile basic setup

--- flask-tutorial/app/models.py
+++ flask-tutorial/app/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
+from hashlib import md5
 from flask_login import UserMixin
 
 
@@ -19,6 +20,10 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
 
 
 @login.user_loader
