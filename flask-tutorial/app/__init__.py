2c6509a046d81ec64a1e6051c97bc5c7aa02b7ae
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2018-12-08 16:21:06 -0800

    Logging for application

--- flask-tutorial/app/__init__.py
+++ flask-tutorial/app/__init__.py
@@ -1,5 +1,6 @@
 import logging
-from logging.handlers import SMTPHandler
+import os
+from logging.handlers import SMTPHandler, RotatingFileHandler
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -29,5 +30,22 @@ if not app.debug:
         )
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
+
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler(
+        'logs/flasktut.py',
+        maxBytes=10240,
+        backupCount=10
+    )
+    file_handler.setFormatter(
+        logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+        )
+    )
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Flasktut startup')
 
 from app import routes, models, errors
