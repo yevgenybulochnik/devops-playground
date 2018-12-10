9011b6d42f46fdb6760d77d7f6125fde20fbb220
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2018-12-10 14:49:49 -0800

    Post form, route + template update

--- flask-tutorial/app/routes.py
+++ flask-tutorial/app/routes.py
@@ -3,8 +3,8 @@ from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 from datetime import datetime
 from app import app, db
-from app.forms import LoginForm, RegistartionForm, EditProfileForm
-from app.models import User
+from app.forms import LoginForm, RegistartionForm, EditProfileForm, PostForm
+from app.models import User, Post
 
 # urllib.parse.unquote_plus required when flask is behind an apache server,
 # this function removes the %2F encoding added to the next parameter
@@ -18,21 +18,19 @@ def before_request():
         db.session.commit()
 
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': "It's a beautiful day in portland"
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': "The Avengers movie was awesome!"
-        }
-    ]
-    return render_template('index.html', title='Home', posts=posts)
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
+    posts = current_user.followed_posts().all()
+    return render_template('index.html', title='Home', form=form, posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
