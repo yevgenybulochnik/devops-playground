31eaea5cfa19932413504580bcd45cc59b8ef3ba
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2018-12-13 14:23:44 -0800

    User password reset

--- flask-tutorial/app/models.py
+++ flask-tutorial/app/models.py
@@ -1,8 +1,10 @@
 from datetime import datetime
-from app import db, login
+from app import app, db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from hashlib import md5
 from flask_login import UserMixin
+from time import time
+import jwt
 
 followers = db.Table(
     'followers',
@@ -61,6 +63,23 @@ class User(UserMixin, db.Model):
         )
         own = Post.query.filter_by(user_id=self.id)
         return followed.union(own).order_by(Post.timestamp.desc())
+
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256',
+        ).decode('utf-8')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(
+                token, app.config['SECRET_KEY'],
+                algorithm=['HS256']
+            )['reset_password']
+        except:
+            return
+        return User.query.get(id)
 
 
 @login.user_loader
