0b4746e4018b212e1eafbfec749bc09cb87a35c5
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2018-12-21 11:53:56 -0800

    Error template main.index issue, issue with auth prefix

--- flask-tutorial/app/__init__.py
+++ flask-tutorial/app/__init__.py
@@ -24,7 +24,7 @@ from app.errors import bp as error_bp
 app.register_blueprint(error_bp)
 
 from app.auth import bp as auth_bp
-app.register_blueprint(auth_bp, url_prefix='/auth')
+app.register_blueprint(auth_bp)
 
 from app.main import bp as main_bp
 app.register_blueprint(main_bp)
